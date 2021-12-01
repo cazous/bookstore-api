985101bb3f5cf9b7bd945d4402e20e231043b0ea
Cazous <caiolspinheiro@gmail.com>
2021-12-01 15:29:27 -0300

    Bookstore v1.69
    Add: CrossOrigin notation em Categoria Resource/ BD File

--- src/main/java/com/bookstore/bookstore/resources/CategoriaResource.java
+++ src/main/java/com/bookstore/bookstore/resources/CategoriaResource.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import com.bookstore.bookstore.dtos.CategoriaDTO;
 import com.bookstore.bookstore.model.entities.Categoria;
 import com.bookstore.bookstore.service.CategoriaService;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping(path = "/categoria")
 public class CategoriaResource {
@@ -34,6 +36,7 @@ public class CategoriaResource {
 		Categoria obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
+	
 	
 	@GetMapping
 	public ResponseEntity<List<CategoriaDTO>> findAll(){
